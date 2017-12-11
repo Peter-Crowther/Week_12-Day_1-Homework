cb08f28c849ef9199ac097722cb2de1a59670fcc
Peter Crowther <petercrowther90@hotmail.com>
2017-12-11 15:56:42 +0000

    rough and ready code working

---
+++ public/app.js
@@ -0,0 +1,28 @@
+var app = function() {
+
+  const ul = document.createElement("ul");
+  ul.classList.add("cat");
+
+  const liName = document.createElement("li");
+  liName.innerText = "Name: Otto";
+
+  const liFood = document.createElement("li");
+  liFood.innerText = "Favourite food: Pete's hand";
+
+  const liImage = document.createElement("li")
+
+  const image = document.createElement("img")
+  image.src = "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb"
+  image.width = 500;
+
+  liImage.appendChild(image);
+  liFood.appendChild(liImage);
+  liName.appendChild(liFood);
+  ul.appendChild(liName);
+
+  const cats = document.querySelector("#cats");
+  cats.appendChild(ul);
+
+};
+
+document.addEventListener('DOMContentLoaded', app);
