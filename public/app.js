4df0d6ea5d50a3e02e34bf6ca8689a691f4693b7
Peter Crowther <petercrowther90@hotmail.com>
2017-12-11 16:16:00 +0000

    rough and ready code complete

--- public/app.js
+++ public/app.js
@@ -15,14 +15,26 @@ var app = function() {
   image.src = "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb"
   image.width = 500;
 
-  liImage.appendChild(image);
-  liFood.appendChild(liImage);
-  liName.appendChild(liFood);
-  ul.appendChild(liName);
+  // liImage.appendChild(image);
+  // liFood.appendChild(liImage);
+  // liName.appendChild(liFood);
+  // ul.appendChild(liName);
 
   const cats = document.querySelector("#cats");
-  cats.appendChild(ul);
+  // cats.appendChild(ul);
 
+  const createImage = function(image) {
+    let image = document.createElement("img")
+
+  }
+
+  const appendElements = function(cats, image, liImage, liFood, liName, ul) {
+    liImage.appendChild(image);
+    liFood.appendChild(liImage);
+    liName.appendChild(liFood);
+    ul.appendChild(liName);
+    cats.appendChild(ul);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', app);
